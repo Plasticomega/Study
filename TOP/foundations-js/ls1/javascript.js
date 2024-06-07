f55cdc11e2bbfa678e726402a9594d65a9641b72
plasticomega <105556394+Plasticomega@users.noreply.github.com>
2024-06-07 16:35:40 +0530

    studied lesson about variables and operators

---
+++ TOP/foundations-js/ls1/javascript.js
@@ -0,0 +1,41 @@
+/*  let age='25'
+    ,name='prakash'
+    ,sex='patanahi';
+alert(age);
+console.log(name);
+sex='2 baar';
+alert(sex);
+a = 10;
+b = 20;
+let x = a + b;
+// alert(x);
+let n=10 , p=3 , t = n/p ;
+alert(t);
+
+a++;
+alert(a);
+b--;
+alert(b);
+
+let next = (100 + 50) * 3;
+alert(next);
+let l = 100 / 50 * 3;
+alert(l)*/
+/*
+let x = -1;
+let y = 0;
+let z = x / y;
+alert(z);
+
+let myNumber = 32;
+myNumber.toString(32);
+myNumber.toString(16);
+myNumber.toString(12);
+myNumber.toString(10);
+myNumber.toString(8);
+p =myNumber.toString(2);
+alert(p);*/
+
+let myNumber = "74";
+myNumber += 3;
+alert(myNumber)
