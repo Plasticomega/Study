3077a12cb1bc1bd0dd8cf64fb5a0083d80c68c45
plasticomega <105556394+Plasticomega@users.noreply.github.com>
2024-06-08 17:45:39 +0530

    studied string

---
+++ TOP/foundations-js/ls2/javascript.js
@@ -0,0 +1,32 @@
+const string = "The revolution will not be televised.";
+console.log(string);
+
+const single = 'Single quotes';
+const double = "Double quotes";
+const backtick = `Backtick`;
+
+console.log(single);
+console.log(double);
+console.log(backtick);
+
+
+
+const name = "champu"
+const marks = "56"
+const totalmarks = "600"
+const output = `maze naav ${name} mala dhaavit padle ${(marks/totalmarks)*100} takke atta ghari sangitlyavar Anna kesa kadtil`;
+alert(output)
+
+
+const maggi = `
+
+
+
+                            HOW TO MAKE MAGGI
+                Step 1:- Unlock your phone
+                Step 2:- Open play store
+                Step 3:- Search and install swiggy
+                Step 4:- Open swiggy and login with your number and 
+                correct address
+                Step 5:- Order maggi , And Enjoy \"PUSSIES!!!!!!\"`
+console.log(maggi)
