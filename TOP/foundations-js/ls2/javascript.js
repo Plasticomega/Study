09cddf4d05d206e06703b3274067435f464433de
plastic <promotionplastic13@gmail.com>
2024-07-28 20:58:19 +0530

    data types

--- TOP/foundations-js/ls2/javascript.js
+++ TOP/foundations-js/ls2/javascript.js
@@ -34,7 +34,6 @@ if (hour>9){
     console.log("balls are true");
 }else{
     console.log("balls are false");}
-*/
 
 x = -3
 if(x % 2 == 0){
@@ -43,3 +42,58 @@ if(x % 2 == 0){
     alert(`The number ${x} is Odd`)
 }
 
+*/
+/**
+ * ===== Troubleshooting =====
+ * The function below should log the number 2, however it does not, 
+ * see if you can fix it!
+ * Be sure to fix it in the spirit of the code, do not hard code the result.
+
+
+function troubleshooting() {
+	const a = 1;
+	const b = 1;
+
+	let result;
+
+	// Edit between these lines
+	// =================================
+	result = a + b;
+
+	// =================================
+
+	return result;
+}
+
+// Do not change this 
+console.log(troubleshooting()) */
+
+/**
+ * The code below tells the browser to ask you for a number
+ * then if that number is `6`, it returns `true` otherwise it returns `false`
+ * 
+ * Change this code so it returns `true` 
+ * when the number is greater 
+ * than or equal to 10, and false if 
+ * it is less than 10
+ 
+
+number = Number(prompt("enter a number"));
+
+function numberChecker() {
+	if(number >= 10) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+console.log(numberChecker(5))*/
+
+const a = 1+8
+const b = 22*3
+const c = 5 % 4
+const d = a - 17
+const e = a+b+c+d
+
+console.log(a,b,c,d,e)
