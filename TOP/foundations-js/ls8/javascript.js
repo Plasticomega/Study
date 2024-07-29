996744d8b5f3903b50d8fc2d7ea2ae9af96a7bf1
plastic <promotionplastic13@gmail.com>
2024-07-30 01:23:51 +0530

    arrays

---
+++ TOP/foundations-js/ls8/javascript.js
@@ -0,0 +1,12 @@
+const bitch = ['start','balls','cars','poggers']
+console.log(bitch[2])
+
+const cats = ["janks","tokii","bokki"]
+
+
+function getLast(string){
+    return string[string.length-1]
+}
+
+console.log(cats.map(getLast))
+
