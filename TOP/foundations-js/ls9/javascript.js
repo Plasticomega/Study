22ced7008816f3d252da78aa1dda221d98cae278
plastic <promotionplastic13@gmail.com>
2024-07-31 23:40:11 +0530

    Dom manipulation

---
+++ TOP/foundations-js/ls9/javascript.js
@@ -0,0 +1,26 @@
+/*const chillan = document.querySelector('.chinese')
+const content = document.createElement("div");
+content.classList.add("content")
+content.textContent = "This is the balls!!"
+
+chillan.appendChild(content);
+
+
+const div = document.createElement("div","gotya")
+console.log(div.getAttribute("p"))
+console.log(div)
+
+
+console.log(document.querySelector(".id"))
+
+div.innerHTML = "<span>Hellow world</span>"*/
+
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+console.log(content)
+
+container.appendChild(content);
