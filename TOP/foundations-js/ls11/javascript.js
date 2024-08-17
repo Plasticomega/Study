0798d28f90d45022526510e0adf6cad6509daee9
plastic <promotionplastic13@gmail.com>
2024-08-17 15:01:28 +0530

    DOM click events

---
+++ TOP/foundations-js/ls11/javascript.js
@@ -0,0 +1,4 @@
+let boong = document.querySelector('.boong');
+
+boong.addEventListener('mouseover',()=>{boong.style.color = 'red'})
+boong.addEventListener('mouseout', ()=> {boong.style.color = 'white'})
