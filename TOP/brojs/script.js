b983fa01d974c78bc70024f728195c6c69598e26
plastic <promotionplastic13@gmail.com>
2025-02-02 23:35:38 +0530

    study

--- TOP/brojs/script.js
+++ TOP/brojs/script.js
@@ -1,2 +1,12 @@
-let age = 25;
-console.log(`Age is of the data-type: ${typeof age}`)
+let increment = document.querySelector(".increment")
+let count = document.querySelector(".count")
+let decrease = document.querySelector(".decrease")
+let currentcount = 0
+increment.addEventListener("click",()=>{
+    currentcount += 1
+    count.textContent = currentcount
+})
+decrease.addEventListener("click",()=>{
+    currentcount -= 1
+    count.textContent =currentcount
+})
