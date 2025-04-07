6ee8dcf5fcbd9158a3767b28805de60ab91fef9a
plastic <promotionplastic13@gmail.com>
2025-04-07 23:30:06 +0530

    text styles

---
+++ cpp/hello.cpp
@@ -0,0 +1,24 @@
+#include <iostream> //comment
+
+int main(){
+    //std::cout << "Hello world!" << '\n';
+    //std::cout << "Fuck you";
+
+    //Variables
+
+    int x; // decleration of variable
+    x = 1;
+    int y;
+    y = 10;
+    int sum = x + y;
+    std::cout << sum;
+
+    //double can be a number including a decimal
+    //char stores single character
+    
+    //boolean (true or false) syntax is bool
+    //string (sequence of text) requires std namespace
+
+    std::string name = "Bro";
+    return 0;
+}
