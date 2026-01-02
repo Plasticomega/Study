914fa57f1ccc964a93615747fa89b164a69f00a4
plastic <promotionplastic13@gmail.com>
2026-01-02 12:05:58 +0530

    js

---
+++ TOP/js/factory/script.js
@@ -0,0 +1,31 @@
+function Add(firstNumber){
+    const first = firstNumber
+    return function secondNumber(secondNumber){
+        const second = secondNumber
+        return first + second
+    }
+}
+
+const add10 = Add(10)
+
+console.log(add10(50))
+
+
+const array = ["mango","banana"]
+
+const [ firstEle, zerothEle] = array;
+
+console.log(firstEle,zerothEle)
+
+
+/*
+ Ground rules for modules
+ - Self contained module - everything to do with module is inside moudle
+ - No global variables 
+ - Cannot manage more than one thing
+ -Separation of concerns
+ -DRY code DONT REPEAT YOURSELF
+ - efficient DOM usage
+ (very few selections)
+ -no memory leaks 
+*/
---
+++ TOP/js/organizing-js-code/objects/script.js
@@ -0,0 +1,65 @@
+// const variable = 'property';
+
+// function Player(name, marker) {
+//   if (!new.target) {
+//     throw Error("You must use the 'new' operator to call the constructor");
+//   }
+//   this.name = name;
+//   this.marker = marker;
+//   this.sayName = function() {
+//     console.log(this.name)
+/* This is how you can create new objects using constructors also the !new.target is to
+catch errors when you don't use the new keyword while generating new constructor*/
+//   };
+// }
+
+// function Book(name,author,pages,read){
+//     this.name = name;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.info = function() {
+//         return `${this.name} by ${this.author}, ${this.pages} pages, ${this.read}`
+//     }
+// }
+
+// let pop = new Book('The adventures of pop','Tim','290','Not read')
+
+// Object.getPrototypeOf(pop) === Book.prototype;
+
+// function Person(name) {
+//   this.name = name;
+// }
+
+// Person.prototype.sayName = function() {
+//   console.log(`Hello, I'm ${this.name}!`);
+// };
+
+// function Player(name, marker) {
+//   this.name = name;
+//   this.marker = marker;
+// }
+
+// Player.prototype.getMarker = function() {
+//   console.log(`My marker is '${this.marker}'`);
+// };
+
+// Object.getPrototypeOf(Player.prototype); // returns Object.prototype
+
+// // Now make `Player` objects inherit from `Person`
+// Object.setPrototypeOf(Player.prototype, Person.prototype);
+// Object.getPrototypeOf(Player.prototype); // returns Person.prototype
+
+// const player1 = new Player('steve', 'X');
+// const player2 = new Player('also steve', 'O');
+
+// player1.sayName(); // Hello, I'm steve!
+// player2.sayName(); // Hello, I'm also steve!
+
+// player1.getMarker(); // My marker is 'X'
+// player2.getMarker(); // My marker is 'O'
+
+//you can make an object inherit another objects properties in this way
+
+
+
